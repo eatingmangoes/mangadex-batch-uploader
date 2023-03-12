86fd803c752b813c9537278a96fa1d3ecd17d2bc
PHDinbeingLAZY <ruparap123@gmail.com>
2023-03-12 09:05:11 +0400

    Update batchuploader.py

--- batchuploader.py
+++ batchuploader.py
@@ -16,29 +16,29 @@ headers={'Authorization': f'Bearer {token}'}
 mangadetails = {
   "manga": "type_your_manga_id",
   "groups": [
-    "type_your_group_i"
+    "type_your_group_id"
   ]
 }
-r=requests.post(f"{API_URL}/upload/begin",headers = headers, json=mangadetails)
-sessionid = r.json()['data']['id']
 
-directory_path = "type_your_folder_directory" #if using windows, use \\ instead of \
+directory_path = "type_your_directory" #if using windows, use \\ instead of \
 
 directories = [d for d in os.listdir(directory_path) if os.path.isdir(os.path.join(directory_path, d))]
 
-order = []
-
 for directory in directories:
+  order = []
+  r=requests.post(f"{API_URL}/upload/begin",headers = headers, json=mangadetails)
+  sessionid = r.json()['data']['id']
   files = os.listdir(os.path.join(directory_path, directory))
   for file in files:
-    with open(f'{directory}\\{file}', 'rb') as f: #change the \\ to / if using a mac
+    with open(f'{directory_path}\\{directory}\\{file}', 'rb') as f: #change the \\ to / if using a mac
       data = f.read()
     r = requests.post(f"{API_URL}/upload/{sessionid}", headers=headers, files={"file": data})
     order.append(r.json()['data'][0]['id'])
+    print("chapter : {} page : {}".format(directory,file))
   chapter_draft = {
-    "volume": "None",
+    "volume": None,
     "chapter": "{}".format(directory),
-    "title": "",
+    "title": None,
     "translatedLanguage": "en"
   }
   newdata ={
